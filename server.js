567b79e5859d9097a77bdd8facfa3c67e01fc1ae
Giovannypl <gestionhumanaylogistica@gmail.com>
2026-01-24 03:29:00 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const crypto = require('crypto');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware CRÍTICO
 app.use(express.json());
